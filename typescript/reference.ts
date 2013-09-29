516f8023f0ed3476c2694ec5145e343ece7579c8
basarat <basaratali@gmail.com>
2013-09-29 12:19:37 +1000

    cleanup

--- typescript/reference.ts
+++ typescript/reference.ts
@@ -1,7 +1,6 @@
-/// <reference path="./angularModules.ts" />
+/// <reference path="angularModules.ts" />
 
 //grunt-start
-/// <reference path="angularModules.ts" />
 /// <reference path="controllers/MainController.ts" />
 /// <reference path="vendor.d.ts" />
 //grunt-end
